4cde7aab8c57c4d89a7a664e9c1fb757c562d7d9
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-07-28 17:37:14 +0545

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,21 @@
+# importing required modules from flask
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
+# defining a function for creating app
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "w7g8&*F^&Scq67f^&c"
+
+# import relative modules inside same folder and they are for viewing web content
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix = "/")
+    app.register_blueprint(auth,url_prefix = "/")
+
+    return app
