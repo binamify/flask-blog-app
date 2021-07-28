84dddb93c0aaf952b14cfe1c058b280cf4f5fe4c
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-07-28 17:34:15 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 
+#importing create_app function from self made module i.e webiste folder
+from website import create_app
+
+#Inside __init__.py app is used and it has __name__
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
