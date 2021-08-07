a2ee67fa94ee23ac5b55249874846c21b4e42421
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-08-07 16:08:35 +0545

    !commenting functionality added

--- website/models.py
+++ website/models.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime, default = func.now())
     posts = db.relationship('Post', backref='user', passive_deletes = True)
+    comments = db.relationship('Comment', backref='user', passive_deletes = True)
 
 
 class Post(db.Model):
@@ -25,3 +26,12 @@ class Post(db.Model):
     text = db.Column(db.Text, nullable = False)
     date_created = db.Column(db.DateTime(timezone = True), default = func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable = False)
+    comments = db.relationship('Comment', backref='post', passive_deletes = True)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    text = db.Column(db.String(150), nullable = False)
+    date_created = db.Column(db.DateTime(timezone = True), default = func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable = False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable = False)
